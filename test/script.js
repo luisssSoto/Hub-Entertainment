43e43ae8012b613c01404c211e291f3689272a7f
Luis Soto <123508182+luisssSoto@users.noreply.github.com>
2024-06-05 14:25:12 -0600

    Delete test directory

--- test/script.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-    var movies = document.querySelectorAll('.movie');
-    movies.forEach(function(movie) {
-        movie.addEventListener('click', function() {
-            var movieId = this.getAttribute('data-id');
-            window.location.href = 'details.html?id=' + movieId;
-        });
-    });
-});
