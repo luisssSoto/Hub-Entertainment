249015c5e992789b793d116a69d99d72e5e6abf3
Luis Soto <alejandrosotoserafin@gmail.com>
2024-06-06 14:06:35 -0600

    addButtonWorking

--- scripts/details.js
+++ scripts/details.js
@@ -1,4 +1,17 @@
-//create all the content related to the movie/serie according the parameters got
+// function to get the list of movies from localStorage
+function getFavoriteMovies() {
+    let movies = localStorage.getItem('favoriteMovies');
+    //JSON -> object JS
+    return movies ? JSON.parse(movies) : [];
+};
+
+// function to save the list of movies to localStorage
+function saveFavoriteMovies(movies) {
+    //object JS -> JSON
+    localStorage.setItem('favoriteMovies', JSON.stringify(movies));
+};
+
+//create all the content related to the movie/serie according the parameters have gotten
 function buildMovie(id, source, alternative, description) {
     let titleElement = document.getElementById('title-movie');
     titleElement.textContent = alternative;
@@ -28,8 +41,9 @@ function buildMovie(id, source, alternative, description) {
     movieContainerElement.appendChild(buttonContainerElement);
 };
 
+
 //consume data from .json file according to the category
-function loadingDetails(){
+function loadingDetails() {
     console.log('loadingDetails is running');
     let params = new URLSearchParams(window.location.search);
     console.log(`params ${params}`);
@@ -45,14 +59,48 @@ function loadingDetails(){
 
     console.log('Category: ' + category);
 
+    let addButtonElement = document.getElementById('add-favorites');
+    let hideButtonElement = document.getElementById('hide-movie');
+
     fetch(category).then(response => response.json()).then(function (output) {
         for (let item of output.data) {
             if (movieId == item.id) {
-                buildMovie(movieId, item.resource, item.title, item.description);
+                buildMovie(item.id, item.resource, item.title, item.description);
+
+                // adding a movie to favorite section
+                addButtonElement.addEventListener('click', function () {
+                    let newTitle = { "id": item.id, "title": item.title, "resource": item.resource, "description": item.description };
+                    let favoriteMovies = getFavoriteMovies();
+                    favoriteMovies.push(newTitle);
+                    saveFavoriteMovies(favoriteMovies);
+                    alert(`Your choice was saved in Favorites sections successfully`);
+                });
+                                
+                //hide a movie of all the main menu (home)
+                hideButtonElement.addEventListener('click', function(){
+                    console.log('hidding...');
+                    let categoryList = category;
+                    console.log('output ' + output);
+                    console.log('data ' + output.data);
+                    console.log('item' + item);
+                    console.log(item.id);
+                    let favoriteMovies = getFavoriteMovies();
+                    for (const key in object) {
+                        if (Object.hasOwnProperty.call(object, key)) {
+                            const element = object[key];
+                            
+                        }
+                    }
+                    favoriteMovies.splice(index, 1);
+                    saveFavoriteMovies(favoriteMovies);
+                    displayFavoriteMovies();
+                    // saveFavoriteMovies(favoriteMovies);
+                    
+                });
+                // console.log('This is the index I\'ve been looking forward:' + data.indexOf(item.id));
             }
         }
     }).catch(function (error) {
         console.log('error: ', error);
     })
-
 };
--- scripts/home-script.js
+++ scripts/home-script.js
@@ -1,10 +1,56 @@
 // Setting the url according to the element selected
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
     let movies = document.querySelectorAll('.video-cards');
-    movies.forEach(function(movie) {
-        movie.addEventListener('click', function() {
+    movies.forEach(function (movie) {
+        movie.addEventListener('click', function () {
             let movieId = this.getAttribute('id');
             window.location.href = 'details.html?id=' + movieId;
         });
     });
 });
+
+// function to get the list of movies from localStorage
+function getFavoriteMovies() {
+    let movies = localStorage.getItem('favoriteMovies');
+    //JSON -> object JS
+    return movies ? JSON.parse(movies) : [];
+}
+
+//Show the list of Favorite Section
+function displayFavoriteMovies() {
+    let favoritesMovieList = document.getElementById('favorites-list'); //ul
+    favoritesMovieList.innerHTML = '';
+    let favoriteMovies = getFavoriteMovies();
+
+    favoriteMovies.forEach((movie) => {
+        let containerImgMovieElement = document.createElement('li');
+        containerImgMovieElement.setAttribute('class', "card-box");
+
+        let imgMovieElement = document.createElement('img');
+        imgMovieElement.setAttribute("class", "video-cards");
+        imgMovieElement.setAttribute("id", movie.id);
+        imgMovieElement.setAttribute("src", movie.resource);
+        imgMovieElement.setAttribute("alt", movie.title);
+
+        containerImgMovieElement.appendChild(imgMovieElement);
+        favoritesMovieList.appendChild(containerImgMovieElement);
+
+        imgMovieElement.addEventListener('click', function(){
+            let movieId = this.getAttribute('id');
+            window.location.href = 'details.html?id=' + movieId;
+        });
+    })
+
+    // favoriteMovies.forEach((movie, index) => {
+    //     const movieItem = document.createElement('li');
+    //     movieItem.textContent = `${movie.title} (Director: ${movie.director}, Año: ${movie.year}) `;
+
+    //     const removeButton = document.createElement('button');
+    //     removeButton.textContent = 'Eliminar';
+    //     removeButton.onclick = () => removeMovie(index);
+
+    //     movieItem.appendChild(removeButton);
+    // });
+}
+
+window.onload = displayFavoriteMovies;
